3e9fd0008d5270df07ae143d21c1a5724c9b4e7e
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-24 16:25:28 +0200

    da finire

---
+++ gioco pari o dispari/main.js
@@ -0,0 +1,17 @@
+//chiedi all'utente un numero tra 1 e 5
+var numero_utente = parseInt(prompt('Inserisci un numero compreso tra 1 e 5'));
+console.log(numero_utente);
+if(numero_utente > 5) {
+    console.log('Hai inserito un numero maggiore di 5! Hai perso!')
+}
+var numero_computer = (Math.floor(Math.random() * 5) + 1);
+console.log(numero_computer);
+var scelta_utente = prompt('Scegli: Pari o dispari?');
+console.log(scelta_utente);
+var somma = numero_utente + numero_computer;
+console.log(somma);
+if (somma % 2 == 0 ) {
+    console.log('La somma è un numero pari');
+} else {
+    console.log('La somma è un numero dispari');
+}
