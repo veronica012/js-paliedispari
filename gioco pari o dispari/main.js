5084e89d37a25a5a553c8ea37957ba33968d16fe
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-24 17:26:13 +0200

    pari o dispari aggiungere funzioni

--- gioco pari o dispari/main.js
+++ gioco pari o dispari/main.js
@@ -4,14 +4,24 @@ console.log(numero_utente);
 if(numero_utente > 5) {
     console.log('Hai inserito un numero maggiore di 5! Hai perso!')
 }
-var numero_computer = (Math.floor(Math.random() * 5) + 1);
-console.log(numero_computer);
 var scelta_utente = prompt('Scegli: Pari o dispari?');
 console.log(scelta_utente);
+scelta_utente = scelta_utente.toLowerCase();
+var numero_computer = (Math.floor(Math.random() * 5) + 1);
+console.log(numero_computer);
 var somma = numero_utente + numero_computer;
 console.log(somma);
-if (somma % 2 == 0 ) {
-    console.log('La somma è un numero pari');
+if (somma % 2 == 0) {
+    if ( scelta_utente == 'pari') {
+        console.log('hai vinto');
+    } else {
+        console.log('hai perso');
+    }
 } else {
-    console.log('La somma è un numero dispari');
+    if ( scelta_utente == 'dispari') {
+        console.log('hai vinto');
+    } else {
+        console.log('hai perso');
+    }
+
 }
