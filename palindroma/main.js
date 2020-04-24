5b203e5911b38ac54ebc461c66faed9cc2ce581b
Veronica Celseti <veronicacelseti12@gmail.com>
2020-04-24 15:36:21 +0200

    funzione check for palindrome

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,13 +1,20 @@
-var parola = prompt('Inserisci una parola');
-console.log(parola);
-var lettere = parola.split('');
-console.log(lettere);
-var lettere_reverse = lettere.reverse();
-console.log(lettere_reverse);
-var parola_join = lettere_reverse.join('');
-console.log(parola_join);
-if (parola != parola_join) {
+var parola_utente = prompt('Inserisci una parola');
+console.log(parola_utente);
+var parola_palindroma = palindroma(parola_utente);
+
+if (parola_utente != parola_palindroma) {
     console.log('La parola non è palindroma');
 } else {
     console.log('La parola è palindroma')
+}
+
+function palindroma(parola) {
+    var lettere = parola.split('');
+    console.log(lettere);
+    var lettere_reverse = lettere.reverse();
+    console.log(lettere_reverse);
+    var parola_join = lettere_reverse.join('');
+    console.log(parola_join);
+
+    return parola_join;
 }
